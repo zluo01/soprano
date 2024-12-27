410c8ac940e0d432fffce2a5b4a04e446174a1c3
zluo01 <potterx20a@gmail.com>
2024-12-26 18:57:44 -0800

    Add build script to build all resource.

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Exit script on any error
+set -e
+
+# Define paths
+FRONTEND_DIR="frontend"
+MAVEN_PROJECT_DIR=$(pwd)
+
+echo "Cleanup..."
+mvn -q clean
+
+echo "Building web UI package..."
+
+# Navigate to the frontend directory
+if [ -d "$FRONTEND_DIR" ]; then
+  cd "$FRONTEND_DIR"
+  # Install npm dependencies
+  echo "Installing npm dependencies..."
+  npm install --silent
+  # Build the web UI package
+  echo "Building the web UI package..."
+  npm run build --silent
+  # Return to the Maven project directory
+  cd "$MAVEN_PROJECT_DIR"
+else
+  echo "Error: Frontend directory '$FRONTEND_DIR' does not exist."
+  exit 1
+fi
+
+
+echo "Building Maven project..."
+
+mvn -q install
+
+echo "Build completed successfully!"

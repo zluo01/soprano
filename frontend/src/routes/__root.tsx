6b26e99e0bdd14e6f71d0e55c69ba14fdfebd23c
zluo01 <potterx20a@gmail.com>
2025-05-24 18:34:59 -0700

    Fix route type reference.

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -4,12 +4,15 @@ import Preview from '@/components/preview';
 import PlaybackQueue from '@/components/queue';
 import Tabs from '@/components/tabs';
 import { Toaster } from '@/components/ui/sonner.tsx';
+import type { QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Suspense } from 'react';
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
   component: () => (
     <>
       <div className="h-full w-screen scroll-smooth pt-[env(safe-area-inset-top)] select-none">
